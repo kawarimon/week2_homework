1023f75fcf9bcb34f5870fce7c6954625d4abcb9
kawarimon <gods.are.220121@gmail.com>
2024-12-12 15:43:53 +0900

    課題E_修正2

--- web_api.py
+++ web_api.py
@@ -5,16 +5,16 @@ import time
 # IDを取得する
 top_stories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 response = requests.get(top_stories_url)
-top_stories = response.json()[:30]
+top_stories = response.json()
 
-time.sleep(1)
 
 # 結果リスト
 news_list = []
 
 # トップ10件のニュースを取得する
-for story_id in top_stories:
+for story_id in top_stories[:30]:
     # 必要に応じて数を変更
+    time.sleep(1)
     story_url = f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json"
     story_response = requests.get(story_url)
     story_data = story_response.json()
@@ -25,8 +25,6 @@ for story_id in top_stories:
     else:
         # タイトルはあるけど、リンクがない場合
         news_list.append({"title": story_data.get("title", "No Title"), "link": None})
-
-    time.sleep(1)
 
 for news in news_list:
     print(news)
